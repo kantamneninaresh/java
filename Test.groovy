f54cd5720afbc00c632da3521d209040a4034960
Naresh <36105610+kantamneninaresh@users.noreply.github.com>
2018-12-04 21:36:08 +0530

    Create Test.groovy

---
+++ Test.groovy
@@ -0,0 +1,9 @@
+def call(datacenter, Closure body) {
+    static final String VAULT_APPSECRET_KEY_PREFIX = 'tm.vault.appsecret.'
+
+    withCredentials([string(credentialsId: VAULT_APPSECRET_KEY_PREFIX + datacenter, variable: 'SECRET_ID')]) {
+        withEnv(['VAULT_TM_ROLE_ID=TM', "VAULT_TM_SECRET_ID=$SECRET_ID"]) {
+            body()
+        }
+    }
+}
