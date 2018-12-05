b031a4a2d7696b4a135a533d77c8d7464488253f
nkantamn <nkantamn@cisco.com>
2018-12-05 18:48:27 +0530

    test

---
+++ pipeline/Jenkinsfile.appdeploy.groovy
@@ -0,0 +1,42 @@
+#!groovy
+
+node("ccone-slave") {
+
+    def mavenHome
+    def deploymentUtils
+
+    final String APP_NAME = 'org-management'
+    final String APP_BASE = 'org-management'
+    final String CF_ORG = 'tenant-management'
+    final String CF_SPACE = 'app'
+    final String DEPLOYMENT = 'dev'
+    final String APP_NAME_PREFIX = 'green-'
+
+    try {
+
+        node("ccone-slave") {
+            stage('Preparation') {
+                checkout scm
+                deploymentUtils = load('pipeline/utils/DeploymentUtils.groovy')
+                mavenHome = tool(name: 'maven-3.5.0', type: 'maven');
+            }
+            withEnv([
+                    'MAVEN_HOME=' + mavenHome,
+                    "PATH=${mavenHome}/bin:${env.PATH}"
+            ]) {
+                //app deploy
+                stage("dev-deploy"){
+                    dir(APP_BASE){
+                        sh "'${mavenHome}/bin/mvn' clean install"
+                        deploymentUtils.appPackageDeploy(DEPLOYMENT, CF_ORG, CF_SPACE,APP_BASE, APP_NAME, APP_NAME_PREFIX)
+                    }
+                }
+                currentBuild.result = "SUCCESS"
+            }
+        }
+    }
+    catch (error) {
+        currentBuild.result = "FAILURE"
+        throw error
+    }
+}
