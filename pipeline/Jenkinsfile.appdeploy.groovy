7632f5d10f51a9c656fbfeaa93fd2a7fd7a6f33e
Naresh <36105610+kantamneninaresh@users.noreply.github.com>
2018-12-05 19:02:10 +0530

    Update Jenkinsfile.appdeploy.groovy

--- pipeline/Jenkinsfile.appdeploy.groovy
+++ pipeline/Jenkinsfile.appdeploy.groovy
@@ -19,16 +19,16 @@
             stage('Preparation') {
                 checkout scm
                 deploymentUtils = load('pipeline/utils/DeploymentUtils.groovy')
-                mavenHome = tool(name: 'maven-3.5.0', type: 'maven');
+                
             }
             withEnv([
-                    'MAVEN_HOME=' + mavenHome,
-                    "PATH=${mavenHome}/bin:${env.PATH}"
+                    
+                    "PATH=${env.PATH}"
             ]) {
                 //app deploy
                 stage("dev-deploy"){
                     dir(APP_BASE){
-                        sh "'${mavenHome}/bin/mvn' clean install"
+                        
                         deploymentUtils.appPackageDeploy(DEPLOYMENT, CF_ORG, CF_SPACE,APP_BASE, APP_NAME, APP_NAME_PREFIX)
                     }
                 }
